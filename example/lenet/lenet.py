9be9e56e789cb7d4345423123605cab9d05c3b07
CtrlX <gitctrlx@gmail.com>
2025-10-10 13:06:06 +0800

    dev

--- example/lenet/lenet.py
+++ example/lenet/lenet.py
@@ -1,101 +1,141 @@
+import argparse
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
 
-# Define modern LeNet model
-class LeNet(nn.Module):
+
+class Net(nn.Module):
     def __init__(self):
-        super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2)  # 32x32x1 -> 32x32x6
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1)             # 32x32x6 -> 28x28x16
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)                   # 2x2 max pooling
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)                              # 6x6x16 = 576 -> 120
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))      # 32x32x6
-        x = self.pool(x)                  # 16x16x6
-        x = self.relu(self.conv2(x))      # 12x12x16
-        x = self.pool(x)                  # 6x6x16
-        x = x.view(-1, 16 * 6 * 6)       # Flatten to 576
-        x = self.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc2(x))
-        x = self.fc3(x)                   # Output logits
-        return x
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
-# Data preparation
-transform = transforms.Compose([
-    transforms.Pad(2),  # Pad to 32x32
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
 
-train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-# Model, loss function, and optimizer
-print("cuda is available:", torch.cuda.is_available())
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cpu')
-model = LeNet().to(device)
-criterion = nn.CrossEntropyLoss()  # Use nn.MSELoss() with one-hot labels if needed
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Training function
-def train(model, train_loader, criterion, optimizer, device):
+def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    running_loss = 0.0
-    correct = 0
-    total = 0
-    
-    for batch_idx, (images, labels) in enumerate(train_loader):
-        images, labels = images.to(device), labels.to(device)
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        outputs = model(images)
-        # loss = criterion(outputs, labels_one_hot)
-        loss = criterion(outputs, labels)
+        output = model(data)
+        loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        running_loss += loss.item()
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        if (batch_idx + 1) % 10 == 0:
-            avg_loss = running_loss / (batch_idx + 1)
-            accuracy = 100 * correct / total
-            print(f"  Batch {batch_idx + 1}/{len(train_loader)} - Loss: {avg_loss:.4f}, Acc: {accuracy:.2f}%")
-            
-    return running_loss / len(train_loader)
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
 
-# Testing function
-def test(model, test_loader, device):
+
+def test(model, device, test_loader):
     model.eval()
+    test_loss = 0
     correct = 0
-    total = 0
     with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    return 100 * correct / total
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-# Training loop
-num_epochs = 5
-for epoch in range(num_epochs):
-    train_loss = train(model, train_loader, criterion, optimizer, device)
-    test_accuracy = test(model, test_loader, device)
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {train_loss:.4f}, Test Accuracy: {test_accuracy:.2f}%')
+    test_loss /= len(test_loader.dataset)
 
-# Save model
-torch.save(model.state_dict(), 'lenet.pth')
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
+                        help='learning rate (default: 1.0)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-accel', action='store_true',
+                        help='disables accelerator')
+    parser.add_argument('--dry-run', action='store_true',
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', 
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+
+    use_accel = not args.no_accel and torch.accelerator.is_available()
+
+    torch.manual_seed(args.seed)
+
+    if use_accel:
+        device = torch.accelerator.current_accelerator()
+    else:
+        device = torch.device("cpu")
+
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_accel:
+        accel_kwargs = {'num_workers': 1,
+                        'persistent_workers': True,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(accel_kwargs)
+        test_kwargs.update(accel_kwargs)
+
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    dataset1 = datasets.MNIST('./data', train=True, download=True,
+                       transform=transform)
+    dataset2 = datasets.MNIST('./data', train=False,
+                       transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
+
+if __name__ == '__main__':
+    main()
